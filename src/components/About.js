e4fb251ac1c19b9cb8c7c4f8067ca7bc0bd288ed
mohankumar67 <Mohankumarwork11@gmail.com>
2025-03-12 15:02:10 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,15 @@ function About () {
     return (
         <section id="about">
             <h2>Hello, I'm <span style={{color:"#ff6f61"}}>Mohan Kumar</span></h2>
-            <p>I am a frontend developer skilled in HTML, CSS, Tailwind CSS, JavaScript, and React.js. And  also i am learning cybersecurity to expand his knowledge in web security</p>
-           <button>View My Work</button>
+               <p>I am a frontend developer skilled in HTML, CSS, Tailwind CSS, JavaScript, and React.js. Additionally, I am exploring cybersecurity to enhance my understanding of web security</p>
+            <a 
+  href="https://github.com/mohankumar67" 
+  target="_blank" 
+  rel="noopener noreferrer"
+  className="button"
+>
+  View My Work
+</a>
         </section>
     );
 }
